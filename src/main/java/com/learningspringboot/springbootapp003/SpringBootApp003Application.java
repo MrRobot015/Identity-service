dda85dff34a73f53da1678707951dfb04120c749
Mujtaba Salah El-Deen <mujtabasalah.dev@gmail.com>
2023-03-30 21:56:31 +0200

    Init project

---
+++ src/main/java/com/learningspringboot/springbootapp003/SpringBootApp003Application.java
@@ -0,0 +1,14 @@
+package com.learningspringboot.springbootapp003;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApp003Application {
+    
+    public static void main(String[] args) {
+        
+        SpringApplication.run(SpringBootApp003Application.class, args);
+    }
+    
+}
---
+++ src/test/java/com/learningspringboot/springbootapp003/SpringBootApp003ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.learningspringboot.springbootapp003;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApp003ApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    
+    }
+    
+}
